2a3b89fb7211e072f7906454ee5ab028a4d7da5e
yuriishymko <yuriishymko@gmail.com>
2021-08-21 11:56:50 +0300

    Add project files.

---
+++ Homework1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int a = 5;
+            Console.WriteLine(a.ToString());
+           
+        }
+    }
+}
---
+++ Homework1/Stack.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1
+{
+    class Stack
+    {
+        private int _arrSize = 0;
+        
+        public int ArrSize
+        {
+            get
+            {
+                return _arrSize;
+            }
+            set
+            {
+                _arrSize = value;
+            }
+
+        }
+       
+
+
+    }
+}
