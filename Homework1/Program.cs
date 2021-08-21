dd0b5b816ed3e67142b8683a5af9d0b533b0b7bb
yuriishymko <yuriishymko@gmail.com>
2021-08-21 11:58:36 +0300

    make cod clear

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -7,10 +7,8 @@ namespace Homework1
     {
         static void Main(string[] args)
         {
-            
             int a = 5;
             Console.WriteLine(a.ToString());
-           
         }
     }
 }
