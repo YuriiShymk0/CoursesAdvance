6dba5e00d07bdb1d1522d380c13ab71b2ecd64df
yuriishymko <yuriishymko@gmail.com>
2021-08-27 00:06:23 +0300

    Homework2

---
+++ homework2/Stack.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+using System.Linq;
+
+namespace Homework2
+{
+    public class Stack<T> where T : struct 
+    {
+        private static T[] _arr = new T[10];
+        private uint countOFOperation = 0;
+
+        public void Push(T value)
+        {
+            if (countOFOperation > (0.8 * (_arr.Length + 1)))
+            {
+                Array.Resize<T>(ref _arr, _arr.Length * 2);
+            }
+            _arr.SetValue(value, Array.IndexOf(_arr, _arr[countOFOperation]));
+            countOFOperation++;
+        }
+
+        public T Pop()
+        {
+            if (countOFOperation > 0)
+            {
+                _arr = _arr.Skip(1).ToArray();
+                countOFOperation--;
+                return Peek();
+            }
+            return default;
+        }
+
+        public T Peek()
+        {
+            return _arr[0];
+        }
+    }
+}
