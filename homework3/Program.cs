25041a2d3493f2f0bdd086ec5be66cc0a71dd679
yuriishymko <yuriishymko@gmail.com>
2021-09-23 00:19:25 +0300

    ready first part of the task

--- homework3/Program.cs
+++ homework3/Program.cs
@@ -5,7 +5,7 @@ namespace homework3
 {
     class Program
     {
-        public static int firstValue, secondValue,result;
+        public static int firstValue, secondValue, result;
         public static char operation;
         delegate int Calculation(int firstValue, int secondValue);
 
@@ -15,52 +15,11 @@ namespace homework3
             secondValue = EnterNumbersFromConsole();
             operation = EnterCharFromConsole();
 
-            Calculation Sum = delegate (int firstValue, int secondValue)
-            {
-                return firstValue + secondValue;
-            };
-            Calculation Subtraction = delegate (int firstValue, int secondValue)
-            {
-                return firstValue - secondValue;
-            };
-            Calculation Multiplication = delegate (int firstValue, int secondValue)
-            {
-                return firstValue * secondValue;
-            };
-            Calculation Division = delegate (int firstValue, int secondValue)
-            {
-                if (secondValue != 0)
-                {
-                    return firstValue / secondValue;
-                }
-                return 0;
-            };
+            CalculationWithanonimDelegate();//anonim methods
 
-            switch (operation)
-            {
-                case '+':
-                    {
-                        Console.WriteLine(TextOnConsole() + Sum(firstValue, secondValue));
-                        break;
-                    }
-                case '-':
-                    {
-                        Console.WriteLine(TextOnConsole() + Subtraction(firstValue, secondValue));
-                        break;
-                    }
-                case '*':
-                    {
-                        Console.WriteLine(TextOnConsole() + Multiplication(firstValue, secondValue));
-                        break;
-                    }
-                case '/':
-                    {
-                        Console.WriteLine(TextOnConsole() + Division(firstValue, secondValue));
-                        break;
-                    }
-            }
-            
+            CalculationWithFuncDelegate();//Func methods
         }
+
         public static int EnterNumbersFromConsole()
         {
             while (true)
@@ -88,27 +47,17 @@ namespace homework3
                     switch (valueFromConsole)
                     {
                         case 1:
-                            {
-                                return '+';
-                            }
+                            return '+';
                         case 2:
-                            {
-                                return '-';
-                            }
+                            return '-';
                         case 3:
-                            {
-                                return '/';
-                            }
+                            return '*';
                         case 4:
-                            {
-                                return '*';
-                            }
+                            return '/';
                         default:
-                            {
-                                Console.Clear();
-                                Console.WriteLine("Try again ");
-                                break;
-                            }
+                            Console.Clear();
+                            Console.WriteLine("Try again ");
+                            break;
                     }
                 }
                 else
@@ -117,8 +66,111 @@ namespace homework3
                 }
             }
         }
-        public static string TextOnConsole() => "Result of Operation: ";
-       
-    }
 
+        public static string TextOnConsole(int forChoose)
+        {
+            if (forChoose == 1)
+            {
+                return "Result of Operation with using anonim methods: ";
+            }
+            return "Result of Operation with using \"Func\" Delegate: ";
+        }
+
+        public static void CalculationWithanonimDelegate()
+        {
+            Calculation Sum = delegate (int firstValue, int secondValue)
+            {
+                return firstValue + secondValue;
+            };
+            Calculation Subtraction = delegate (int firstValue, int secondValue)
+            {
+                return firstValue - secondValue;
+            };
+            Calculation Multiplication = delegate (int firstValue, int secondValue)
+            {
+                return firstValue * secondValue;
+            };
+            Calculation Division = delegate (int firstValue, int secondValue)
+            {
+                if (secondValue != 0)
+                {
+                    return firstValue / secondValue;
+                }
+                Error();
+                return 0;
+            };
+
+            switch (operation)
+            {
+                case '+':
+                    Console.WriteLine(TextOnConsole(1) + Sum(firstValue, secondValue));
+                    break;
+                case '-':
+                    Console.WriteLine(TextOnConsole(1) + Subtraction(firstValue, secondValue));
+                    break;
+                case '*':
+                    Console.WriteLine(TextOnConsole(1) + Multiplication(firstValue, secondValue));
+                    break;
+                case '/':
+                    Console.WriteLine(TextOnConsole(1) + Division(firstValue, secondValue));
+                    break;
+            }
+        }
+
+        public static int Sum(int firstValue, int secondValue)
+        {
+            return firstValue + secondValue;
+        }
+
+        public static int Subtraction(int firstValue, int secondValue)
+        {
+            return firstValue - secondValue;
+        }
+
+        public static int Multiplication(int firstValue, int secondValue)
+        {
+            return firstValue * secondValue;
+        }
+
+        public static int Division(int firstValue, int secondValue)
+        {
+            if (secondValue != 0)
+            {
+                return firstValue / secondValue;
+            }
+            Error();
+            return 0;
+        }
+
+        public static void CalculationWithFuncDelegate()
+        {
+            Func<int, int, int> sum = Sum;
+            Func<int, int, int> subtraction = Subtraction;
+            Func<int, int, int> multiplication = Multiplication;
+            Func<int, int, int> division = Division;
+            switch (operation)
+            {
+                case '+':
+                    Console.WriteLine(TextOnConsole(0) + sum(firstValue, secondValue));
+                    break;
+                case '-':
+                    Console.WriteLine(TextOnConsole(0) + subtraction(firstValue, secondValue));
+                    break;
+                case '*':
+                    Console.WriteLine(TextOnConsole(0) + multiplication(firstValue, secondValue));
+                    break;
+                case '/':
+                    Console.WriteLine(TextOnConsole(0) + division(firstValue, secondValue));
+                    break;
+            }
+        }
+
+        public static void Error()
+        {
+            Console.WriteLine("cannot be divided by zero");
+        }
+        //    Action<string, string> HelloOnConsole;
+        //    HelloOnConsole =>  "Hello World";
+
+    }
 }
