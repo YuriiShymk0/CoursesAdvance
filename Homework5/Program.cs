39f8609058871d4fb7a12d3dc0e0da505e8f267c
yuriishymko <yuriishymko@gmail.com>
2021-09-27 05:04:42 +0300

    ready 5 homework

---
+++ Homework5/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace Homework5
+{
+    class Program
+    {
+        private const int _lvlEncrypt = 6;
+        private static string _stringForDeserialize = "";
+
+        static void Main(string[] args)
+        {
+            User user = new User()
+            {
+                Age = 23,
+                Name = "John",
+                Seniority = "3"
+            };
+
+            string jsonstrforserilialize = JsonSerializer.Serialize<User>(user);
+
+            Console.WriteLine("Serialized user : " + jsonstrforserilialize);
+
+            Console.WriteLine("\nEncrypted Json string : " + EncryptionJson(jsonstrforserilialize));
+
+            Console.WriteLine("\nSerilalized Json file :" + Serilalizer("User.json", jsonstrforserilialize));
+
+            Console.WriteLine("\nDeserilalized Json file :" + Deserilalizer("User.json"));
+
+            Console.WriteLine("\nDecrypted Json string : " + DecryptionJson(_stringForDeserialize));
+
+        }
+
+        private static string Serilalizer(string roadtofile, string dataforserealize)
+        {
+            _stringForDeserialize = JsonSerializer.Serialize(EncryptionJson(dataforserealize));
+            File.WriteAllText(roadtofile, _stringForDeserialize);
+            return _stringForDeserialize;
+        }
+
+        private static string Deserilalizer(string roadtofile)
+        {
+            string jsonstr = File.ReadAllText(roadtofile);
+            _stringForDeserialize = JsonSerializer.Deserialize<string>(jsonstr);
+            return _stringForDeserialize;
+        }
+
+        private static string EncryptionJson(string jsonStrForEncrypt)
+        {
+            string EncryptedStr = "";
+            foreach (var item in jsonStrForEncrypt)
+            {
+                EncryptedStr += (char)((short)item << _lvlEncrypt);
+            }
+            return EncryptedStr;
+        }
+        private static string DecryptionJson(string encryptedjsonStr)
+        {
+            string DecryptedStr = "";
+            foreach (var item in encryptedjsonStr)
+            {
+                DecryptedStr += (char)((short)item >> _lvlEncrypt);
+            }
+            return DecryptedStr;
+        }
+
+    }
+}
---
+++ Homework5/User.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework5
+{
+    class User
+    {
+
+        private string _name;
+        private int _age;
+        private IEnumerable _seniority;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("Name can`t be empty");
+                }
+                _name = value;
+            }
+        }
+        public IEnumerable Seniority
+        {
+            get
+            {
+                return _seniority;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("Seniority can`t be smaller than 0");
+                }
+                _seniority = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                if (value < 18)
+                {
+                    throw new ArgumentNullException("Age can`t be smaller than 18");// course person can get seniority after 18
+                }
+                _age = value;
+            }
+        }
+    }
+}
