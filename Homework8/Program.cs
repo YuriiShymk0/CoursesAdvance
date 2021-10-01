faaad29f45d648508329ce4cb8d50c6ce5889dc6
yuriishymko <yuriishymko@gmail.com>
2021-10-01 21:27:54 +0300

    some part of homework

---
+++ Homework8/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Homework8
+{
+    class Program
+    {
+        static Stack<int> stack = new Stack<int>();
+        static Semaphore semaphore = new Semaphore(5, 5);
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Thread thread = new Thread(HelpPush);
+                thread.Start(i);
+            }
+        }
+        public static void HelpPush()
+        {
+            stack.Push(5);
+        }
+    }
+}
---
+++ Homework8/Stack.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+namespace Homework8
+{
+
+    public class Stack<T> where T : struct
+    {
+        private static T[] _arr = new T[10];
+        private uint countOFOperation = 0;
+        public object locker = new object();
+        
+
+        public void Push(T value)
+        {
+            lock (locker)
+            {
+                if (countOFOperation > (0.8 * (_arr.Length + 1)))
+                {
+                    Array.Resize<T>(ref _arr, _arr.Length * 2);
+                }
+                _arr.SetValue(value, Array.IndexOf(_arr, _arr[countOFOperation]));
+                countOFOperation++;
+            }
+        }
+
+        public T Pop()
+        {
+            lock (locker)
+            {
+                if (countOFOperation > 1)
+                {
+                    _arr = _arr.Where((source, index) => index != countOFOperation - 1).ToArray();
+                    countOFOperation--;
+                    return _arr[countOFOperation - 1];
+                }
+                return default;
+            }
+        }
+
+        public T Peek()
+        {
+            return _arr[0];
+        }
+
+    }
+
+}
