285f69b9fac2ef1db0bd77753b69039fd98a7d0e
yuriishymko <yuriishymko@gmail.com>
2021-10-02 00:57:29 +0300

    do code better

--- Homework8/Program.cs
+++ Homework8/Program.cs
@@ -19,26 +19,28 @@ namespace Homework8
                     Console.WriteLine($"{Thread.CurrentThread.Name} operation Push number ({i})");
                 }
                 );
-                thread.Name = $"Thread № {i} ";
-                thread.Start();
-                Thread.Sleep(50);
+                HelpConsole(thread, i);
             }
             Console.WriteLine();
             for (int i = 0; i < 10; i++)
             {
                 Thread thread1 = new Thread(() => Console.WriteLine($"{Thread.CurrentThread.Name}  operation Pop number ({stack.Pop()})"));
-                thread1.Name = $"Thread № {i} :";
-                thread1.Start();
-                Thread.Sleep(50);
+                HelpConsole(thread1, i);
             }
             Console.WriteLine();
             for (int i = 0; i < 10; i++)
             {
                 Thread thread2 = new Thread(() => Console.WriteLine($"{Thread.CurrentThread.Name} operation Peek ({stack.Peek()})"));
-                thread2.Name = $"Thread № {i} :";
-                thread2.Start();
-                Thread.Sleep(50);
+                HelpConsole(thread2,i);
             }
         }
+        public static void HelpConsole(Thread name, int counter)
+        {
+            name.Name = $"Thread № {counter} :";
+            name.Start();
+            Thread.Sleep(50);
+        }
+
     }
+
 }
