895f4f407c7379bd0dd6c76d2f4a139d0ced1fa5
yuriishymko <yuriishymko@gmail.com>
2021-10-03 22:59:50 +0300

    some part of 9 homewok

---
+++ Homework9/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using Library;
+
+namespace Homework9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MeinMenu());
+        }
+        private static int MeinMenu()
+        {
+            while (true)
+            {
+                Console.WriteLine("Please choose .dll to connection:\n\n1.Press 1 to connect Library1.dll(with serializable User)\n2.Press 2 to connect Library2.dll(with serializable Company)");
+                if (int.TryParse(Console.ReadLine(), out int valueFromConsole ))
+                {
+                    switch (valueFromConsole)
+                    {
+                        case 1:
+                            return 1;
+                        case 2:
+                            return 2;
+                        default:
+                            Console.WriteLine("\t\t\t\t\t\tTry again ");
+                            Thread.Sleep(700);
+                            Console.Clear();
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\t\t\t\t\t\tTry again ");
+                    Thread.Sleep(700);
+                    Console.Clear();
+                }
+            }
+        }
+    }
+}
---
+++ Library1/library1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Library
+{
+    [Serializable]
+    public class User
+    {
+        private string _name;
+        private string _surname;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (value != null)
+                {
+                    _name = value;
+                }
+                else
+                {
+                    throw new ArgumentNullException("Name can`t be empty");
+                }
+            }
+        }
+
+        public string Surname
+        {
+            get
+            {
+                return _surname;
+            }
+            set
+            {
+                if (value != null)
+                {
+                    _surname = value;
+                }
+                else
+                {
+                    throw new ArgumentNullException("Surname can`t be empty");
+                }
+            }
+        }
+    }
+    public class Company
+    {
+        private string _company_name;
+
+        public string CompanyName
+        {
+            get
+            {
+                return _company_name;
+            }
+            set
+            {
+                if (value != null)
+                {
+                    _company_name = value;
+                }
+                else
+                {
+                    throw new ArgumentNullException("CompanyName can`t be empty");
+                }
+            }
+        }
+    }
+}
